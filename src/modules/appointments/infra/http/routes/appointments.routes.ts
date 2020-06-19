5f4e842071288d6b1ae719f87420c5c70b820ce6
Leonardo Menezes <leonhard.menezes@gmail.com>
2020-06-19 15:47:53 -0300

    [Bootcamp GoStack] Prestador de serviços - Verisonando resutado dos testes - Criando controller para a nova rota - Definindo nova rota - Crinado testes para o service de listagem dos agendamentos para o provedor - Crinado service para listar os agendamentos do provedor

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -2,19 +2,16 @@ import { Router } from 'express';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import AppointmentsController from '../controllers/AppointmentsController';
+import ProviderAppointmentsController from '../controllers/ProviderAppointmentsController';
 
 const appointmentsRouter = Router();
 const apointmentsController = new AppointmentsController();
+const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 // executa o middleware ensureAuthenticated toda vez que uma requisição é realizada para o caminho /appointments.
 
-// appointmentsRouter.get('/', async (request, response) => {
-//   const appointments = await appointmentsRepository.find();
-
-//   return response.json(appointments);
-// });
-
 appointmentsRouter.post('/', apointmentsController.create);
+appointmentsRouter.get('/me', providerAppointmentsController.index);
 
 export default appointmentsRouter;
